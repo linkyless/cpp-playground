f974489b94dd1b5b1a3b77dbdbece42908263cc2
linkyless <lestergamer200@gmail.com>
2026-03-27 18:12:15 +0100

    create boid as a triangle movement and wrap-around w/rotation

--- boids/Boids.hpp
+++ boids/Boids.hpp
@@ -1,8 +1,19 @@
 #pragma once
 #include "Vec2.hpp"
+#include <cmath>
 
 struct Boid {
-    Vec2 x;
-    Vec2 v;
-    Boid(Vec2 pos, Vec2 velocity) : x(pos), v(velocity) {}
-}
+    Vec2 position;
+    Vec2 velocity;
+    Boid(Vec2 position, Vec2 velocity) : position(position), velocity(velocity) {}
+    
+    void update(float deltaTime, const float width, const float height) {
+        position = position + velocity * deltaTime;
+        if (position.x > width) position.x = 0.0f;
+        else if (position.x < 0) position.x = width;
+
+        if (position.y > height) position.y = 0.0f;
+        else if (position.y < 0) position.y = height;
+    }
+    
+};
--- boids/main.cpp
+++ boids/main.cpp
@@ -1,15 +1,41 @@
 #include <SFML/Graphics.hpp>
 #include "Vec2.hpp"
+#include "Boids.hpp"
+#include <random>
+
+std::random_device rd;
+std::mt19937 gen(rd());
+std::uniform_int_distribution<> dist(-3, 3);
+
+constexpr float PI = 3.14159f;
+constexpr float kSizeWidth = 800.0f;
+constexpr float kSizeHeight = 600.0f;
 
 int main() {
 
     sf::RenderWindow window(sf::VideoMode({800, 600}), "Boids Sim");
+    sf::Clock clock;
+    Vec2 position(0, 0);
+    Vec2 velocity(180.0f, 180.0f);
+    Boid boid(position, velocity);
+    sf::CircleShape triangle(13.0f, 3);
+    triangle.setPosition({boid.position.x, boid.position.y});
     while (window.isOpen()) {
+        sf::Time dt = clock.restart();
         while (const std::optional event = window.pollEvent()) {
             if (event -> is<sf::Event::Closed>())
                 window.close();
         }
+        
         window.clear(sf::Color::Black);
+        boid.update(dt.asSeconds(), kSizeWidth, kSizeHeight);
+        float angleRad = std::atan2f(boid.velocity.y, boid.velocity.x);
+        float angleDeg = angleRad * 180.0f / PI;
+
+        triangle.setPosition({boid.position.x, boid.position.y});
+        triangle.setRotation(sf::degrees(angleDeg + 90.0f));
+        window.draw(triangle);
         window.display();
+        
     }
 }
